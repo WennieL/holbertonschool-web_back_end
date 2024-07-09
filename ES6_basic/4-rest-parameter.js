05b8bd5f380d5b7d4012b21de510090a05c19036
WennieL <jecxu@hotmail.com>
2024-07-09 15:18:51 +1000

    task 4

--- ES6_basic/4-rest-parameter.js
+++ ES6_basic/4-rest-parameter.js
@@ -1,12 +1,3 @@
 export default function returnHowManyArguments(...input) {
   return input.length;
-  // let sum = 0;
-  // for (const i of input) {
-  //   if (i) {
-  //     sum += 1;
-  //   }
-  // }
-  // return sum;
 }
-
-//  or simply return arguments.length;
