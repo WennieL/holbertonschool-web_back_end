2e421a04f2b5407428e673519c1ecce9d68104b6
WennieL <jecxu@hotmail.com>
2024-07-13 12:37:21 +1000

    task 4

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,12 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  const studentCity = students.filter((student) => student.location === city);
+  const studentGrade = studentCity.map((student) => {
+    const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+    return {
+      ...students,
+      grade: gradeObj ? gradeObj.grade : 'N/A',
+    };
+  });
+
+  return studentGrade;
+}
