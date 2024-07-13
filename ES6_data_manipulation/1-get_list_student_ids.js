e6f6a826ef95b64434367c0365a60dd0c67dc478
WennieL <jecxu@hotmail.com>
2024-07-13 11:19:08 +1000

    task 1 udpate

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
-export default getListStudentIds(students) {
+export default function getListStudentIds(students) {
   if (!Array.isArray(students)) {
     return [];
-  }  
+  }
   return students.map(student => student.id);
 }
