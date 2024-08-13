0a1af89a4aa3d33d98b6d4c998aa6b15a2a8130e
WennieL <jecxu@hotmail.com>
2024-08-13 23:29:57 +1000

    pycodestyle

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-'''Define and annotate the following variables with the specified values:'''
+'''Module define and annotate the following
+variables with the specified values:'''
 
 a: int = 1
 pi: float = 3.14
