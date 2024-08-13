1b54e225c3b648d80560fc06b3cf72d29bfabfa0
WennieL <jecxu@hotmail.com>
2024-08-13 23:33:33 +1000

    task 3

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-'''This module takes a float and returns the string representation of the float'''
+'''This module takes a float and returns
+the string representation of the float'''
 
 
 def to_str(n: float) -> str:
     '''
-    Type-annotated function - to_str 
+    Type-annotated function - to_str
     Arguments: a float n
     returns: string representation of the float.'''
     return str(n)
