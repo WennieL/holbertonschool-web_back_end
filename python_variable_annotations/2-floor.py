93c41341cc880cdf6a7a1fee7cd169e46fc4a08c
WennieL <jecxu@hotmail.com>
2024-08-13 23:15:25 +1000

    fix doc stt

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
 
 def floor(n: float) -> int:
+    '''a type-annotated function floor which takes 
+    a float n as argument and returns the floor of the float.'''
     return int(n)
--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -3,6 +3,9 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    '''a type-annotated function make_multiplier that takes a float 
+    multiplier as argument and returns a function 
+    that multiplies a float by multiplier.'''
     def multiplier_function(value: float) -> float:
         return value * multiplier
     return multiplier_function
