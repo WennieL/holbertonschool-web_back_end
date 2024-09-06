9a6d5a9023624c04c82f034f8dac5a6084eade52
WennieL <jecxu@hotmail.com>
2024-09-07 09:33:42 +1000

    task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,11 @@
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
+  process.exit();
 });
 
-readline.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing\n');
-  readline.close();
+process.on('exit', () => {
+  console.log('This important software is now closing');
 });
