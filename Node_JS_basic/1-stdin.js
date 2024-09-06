c49a64e68fdb853759d0d4965c6e93e670d1bac8
WennieL <jecxu@hotmail.com>
2024-09-07 09:48:31 +1000

    task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,12 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\r\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\r\n`);
-  process.exit();
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`data: Your name is: ${chunk}`);
+  }
 });
 
 process.on('exit', () => {
-  console.log('This important software is now closing\r\n');
+  process.stdout.write('This important software is now closing\n');
 });
