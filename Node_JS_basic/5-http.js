0af088864cc880c30c674f2cd44d6f9262a4009c
WennieL <jecxu@hotmail.com>
2024-09-07 12:36:08 +1000

    task 5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,13 +1,14 @@
 const http = require('http');
-const fs = require('fs');
+const fs = require('fs').promises;
 
 const host = 'localhost';
 const port = 1245;
 
-const filename = process.argv[2];
+const database = process.argv[2];
 
 async function requestListener(req, res) {
-  res.setHeader('Content-type', 'text/plain');
+  res.setHeader('Content-Type', 'text/plain');
+
   switch (req.url) {
     case '/':
       res.writeHead(200);
@@ -17,43 +18,42 @@ async function requestListener(req, res) {
       res.writeHead(200);
       res.write('This is the list of our students\n');
       try {
-        const data = await fs.readFil(filename, 'utf-8');
+        const data = await fs.readFile(database, 'utf8');
         const rows = data.split('\n').slice(1);
 
         const studentsCS = [];
         const studentsSWE = [];
 
         for (const row of rows) {
-          if (row.trim() !== '') {
-            const data = row.split(',');
+          const columns = row.split(',');
 
-            if (data[3] === 'CS') {
-              studentsCS.push(data[0]);
-            }
+          if (columns[3] === 'CS') {
+            studentsCS.push(columns[0]);
+          }
 
-            if (data[3] === 'SWE') {
-              studentsSWE.push(data[0]);
-            }
+          if (columns[3] === 'SWE') {
+            studentsSWE.push(columns[0]);
           }
         }
 
         res.write(`Number of students: ${studentsCS.length + studentsSWE.length}\n`);
         res.write(`Number of students in CS: ${studentsCS.length}. List: ${studentsCS.join(', ')}\n`);
-        res.write(`Number of students in SWE: ${studentsSWE.length}. List: ${studentsSWE.join(', ')}`);
-        res.end();
-      } catch (error) {
+        res.write(`Number of students in SWE: ${studentsSWE.length}. List: ${studentsSWE.join(', ')}\n`);
+        res.end(); 
+      } catch (err) {
         res.end('Cannot load the database');
       }
       break;
     default:
       res.writeHead(404);
-      res.end('Not Found - Hello Holberton School!');
+      res.end('Not Found');
+      break;
   }
 }
 
 const app = http.createServer(requestListener);
 app.listen(port, host, () => {
-  console.log(`Server is running on http://${host}:${port}`);
+  console.log(`Server running at http://${host}:${port}/`);
 });
 
-module.exports = app;
+module.exports = requestListener;
