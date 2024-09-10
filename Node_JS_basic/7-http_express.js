ea643abc36690ed45a4bf29456e3d62523e619d3
WennieL <jecxu@hotmail.com>
2024-09-10 16:02:16 +1000

    Task 7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,52 +1,49 @@
 const express = require('express');
-const fs = require('fs');
+const fs = require('fs').promises;
 
 const app = express();
-const port = 1245;
+const PORT = 1245;
 
-const filename = process.argv[2];
+const database = process.argv[2];
 
+// home
 app.get('/', (req, res) => {
-  res.writeHead(200);
-  res.end('Hello Holberton School!');
+  res.status(200);
+  res.send('Hello Holberton School!');
 });
 
+// students
 app.get('/students', async (req, res) => {
-  res.writeHead(200);
-
+  res.status(200);
+  res.write('This is the list of our students\n');
   try {
-    res.write('This is the list of our students\n');
-
-    const data = await fs.readFileSync(filename, 'utf-8');
+    const data = await fs.readFile(database, 'utf8');
     const rows = data.split('\n').slice(1);
 
     const studentsCS = [];
     const studentsSWE = [];
 
     for (const row of rows) {
-      if (row.trim() !== '') {
-        const data = row.split(',');
+      const columns = row.split(',');
 
-        if (data[3] === 'CS') { // hardcode
-          studentsCS.push(data[0]);
-        }
+      if (columns[3] === 'CS') {
+        studentsCS.push(columns[0]);
+      }
 
-        if (data[3] === 'SWE') { // hardcode
-          studentsSWE.push(data[0]);
-        }
+      if (columns[3] === 'SWE') {
+        studentsSWE.push(columns[0]);
       }
     }
 
     res.write(`Number of students: ${studentsCS.length + studentsSWE.length}\n`);
     res.write(`Number of students in CS: ${studentsCS.length}. List: ${studentsCS.join(', ')}\n`);
     res.end(`Number of students in SWE: ${studentsSWE.length}. List: ${studentsSWE.join(', ')}`);
-  } catch (error) {
+  } catch (err) {
     res.end('Cannot load the database');
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+app.listen(PORT, () => {
 });
 
 module.exports = app;
